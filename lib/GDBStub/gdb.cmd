6cbff50d159dc86d92183fe9519d207f0321be45
Yuanyi Wu <wuyuanyi135@gmail.com>
2020-09-06 00:40:08 -0400

    Debug feature

---
+++ lib/GDBStub/gdb.cmd
@@ -0,0 +1,2 @@
+@echo off
+"C:\Users\wuyua\.platformio\packages\toolchain-xtensa\bin\xtensa-lx106-elf-gdb.exe" -q -x "C:\Users\wuyua\CLionProjects\Julabo_FP50_DS18B20_Control\lib\GDBStub\gdbcmds" %*
